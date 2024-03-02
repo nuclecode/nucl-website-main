f7e7c83df7eb05f71a406c1d4c8d53aabda867b1
idncod <idncod@protonmail.com>
2024-03-02 16:50:23 +0000

    fixed errors

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Accordion, AccordionSummary, AccordionDetails, Typography, Box, Grid } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Typography, Box, Grid } from '@mui/material';
 
 function Contact() {
   return (
@@ -38,50 +37,3 @@ function Contact() {
 }
 
 export default Contact;
-
-{/* <div className="page-container">
-      <h1>Contact Us</h1>
-
-      {/* Contact Info */}
-    //   <div className="contact-info">
-    //     <p>Phone: +44 (0) 7355 573 823</p>
-    //     <p>Email: hire@nuclecode.uk</p>
-    //     {/* Add more contact information as needed */}
-    //   </div>
-
-    //   {/* FAQ Section */}
-    //   <div className="faq-section">
-    //     <h2>FAQ</h2>
-    //     <Accordion>
-    //       <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-    //         <Typography>Question 1</Typography>
-    //       </AccordionSummary>
-    //       <AccordionDetails>
-    //         <Typography>
-    //           Answer to question 1...
-    //         </Typography>  
-    //       </AccordionDetails>
-    //     </Accordion>
-    //     <Accordion>
-    //       <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-    //         <Typography>Question 2</Typography>
-    //       </AccordionSummary>
-    //       <AccordionDetails>
-    //         <Typography>
-    //           Answer to question 2...
-    //         </Typography>  
-    //       </AccordionDetails>
-    //     </Accordion>
-    //     <Accordion>
-    //       <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-    //         <Typography>Question 3</Typography>
-    //       </AccordionSummary>
-    //       <AccordionDetails>
-    //         <Typography>
-    //           Answer to question 3...
-    //         </Typography>  
-    //       </AccordionDetails>
-    //     </Accordion>
-    //   </div>
-    // </div>
-
