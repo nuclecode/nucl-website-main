eaa965928f5538947d8ac69cdaff5403112b8152
idncod <idncod@protonmail.com>
2024-03-03 00:17:19 +0000

    added forms mui

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
-import { Grid, Card, CardContent, Typography, Button } from '@mui/material';
+import { Grid, Card, CardContent, Typography, Button, Box } from '@mui/material';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import TextField from '@mui/material/TextField';
 
 function Home () {
 
     const cardStyles = {
-    borderRadius: 16, // Adjust border radius as needed
+    borderRadius: 16,
     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', // Add box shadow for depth
-    padding: 16, // Add padding for content
+    padding: 16,
   };
     return (
 <div className="home-container">
 <section className="hero-section hero-section-background">
         <div className="hero-banner">
-        <Typography variant="h1">Welcome to Our Website</Typography>
-          <Typography variant="body1">We specialize in providing cutting-edge solutions for your business needs.</Typography>          <Link to="/portfolio" className="pf-btn">Portfolio</Link>
+        <Typography variant="h1">The future of your website secured</Typography>
+          <Typography variant="h6">Get secure and tested code to make your website less prone to exploits, with long-lasting effects</Typography>          <Link to="/portfolio" className="pf-btn">Portfolio</Link>
           <Link to="/schedule-a-call" className="cta-button">Get Started</Link>
         </div>
       </section>
       
-      <section className="services-section" style={{ padding: '0 16px' }}>
-        <h2>Our Services</h2>
+      <section className="services-section" style={{ padding: '0 16px', textAlign: 'center' }}>
+        <h2 style={{ padding:'20px' }}>Our Services</h2>
         <Grid container spacing={3} justifyContent="center">
           {/* Service 1 */}
           <Grid item xs={12} sm={4}>
@@ -89,25 +90,51 @@ function Home () {
         </Grid>
       </section>
       
-      <section className="about-section">
+      <section className="about-section" style={{ padding: '20px', textAlign:'center' }}>
         <h2>About Us</h2>
         <p>Learn more about our company and our mission to help businesses succeed.</p>
         <Link to="/about" className="read-more-link">Read More</Link>
       </section>
       
-      <section className="portfolio-section">
+      <section className="portfolio-section"  style={{ padding: '20px', textAlign: 'center' }}>
         <h2>Our Portfolio</h2>
         <p>Explore some of our past projects and see how we've helped other businesses achieve their goals.</p>
         <Link to="/portfolio" className="view-portfolio-link">View Portfolio</Link>
       </section>
       
-      <section className="contact-section">
-        <h2>Contact Us</h2>
-        <p>Have a project in mind or need assistance? Contact us today!</p>
-        <Link to="/contact" className="contact-us-link">Contact Us</Link>
+      <section className="start-today-section" style={{ padding: '20px', textAlign: 'center' }}>
+        <h2 style={{ padding: '20px' }}>Ready to start a project?</h2>
+        <p>Have a project in mind or need help with an existing one?</p>
+        <Button variant="contained" component={Link} to={'/schedule-a-chat'}>Let's chat!</Button>
+      </section>
+
+      <section className="subscribe-newsletter" style={{ padding: '20px', textAlign: 'center' }}>
+        <h3>Subscribe to our tech newsletter</h3>
+        <p>Freshest from the world of web and cybersecurity straight to your inbox every Thursday.</p>
+        
+        <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <Button variant="contained" component={Link} to={'/terms-and-conditions'}>Sign me up</Button>
+    </Box>
+    <p style={{ fontSize:'13px', padding: '0 20 0 15' }}>
+      By signing up to nuclecode tech newsletter, you agree to the&nbsp;
+      <span style={{ textDecoration: 'none' }}>
+      <Link to="/terms-and-conditions" style={{ textDecoration: 'underline' }}>terms and conditions</Link>
+      </span>
+      .
+    </p>
       </section>
     </div>
     );
+    
 }
+
 
 export default Home;
