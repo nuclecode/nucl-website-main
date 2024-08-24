f6a9ee6d5abbd0ef495dc61b3f0fb69d76c8ab1c
idncod <idncod@protonmail.com>
2024-08-25 00:38:48 +0100

    add fallback favicon to be used as default when browser theme is not detected or cached & add next.config.js for cache expiry for favicon to update whenever a theme changes

---
+++ next.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    async headers() {
+        return [
+            {
+                source: '/:all*(svg)',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-cache, no-store, must-revalidate',
+                    },
+                    {
+                        key: 'Pragma',
+                        value: 'no-cache',
+                    },
+                    {
+                        key: 'Expires',
+                        value: '0',
+                    },
+                ],
+            },
+        ];
+    },
+};
