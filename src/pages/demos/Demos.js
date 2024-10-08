5305295fa5a31c589e612a9dcc499bbc1d9130c6
idncod <idncod@protonmail.com>
2024-10-08 21:39:03 +0100

    refactor demo to have header and footer within <Layout /> & nest the demo in the card

--- src/pages/demos/Demos.js
+++ src/pages/demos/Demos.js
@@ -1,5 +1,6 @@
 import styles from './Demos.module.scss';
 import { FiExternalLink } from 'react-icons/fi';
+import Layout from '../../layout/layout';
 
 const demoWebsites = [
     { name: "e-Commerce Store", url: "https://idncod-store.netlify.app/products" },
@@ -8,31 +9,33 @@ const demoWebsites = [
 
 const Demos = () => {
     const openWebsiteInNewTab = (url) => {
-        window.open(url, '_blank', 'noopener,noreferrer'); // Opens the website in a new tab
+        window.open(url, '_blank', 'noopener,noreferrer');
     };
     return (
-        <div className={styles.demos}>
-            <h1>See how the websites we make look and perform.</h1>
-            <div className={styles.demosGrid}>
-                {demoWebsites.map((demo, index) => (
-                    <div key={index} className={styles.demoCard}>
-                        <h2>{demo.name}</h2>
-                        <iframe
-                            src={demo.url}
-                            title={demo.name}
-                            loading="lazy"
-                            className={styles.demoIframe}
-                            allowFullScreen
-                        ></iframe>
-                        <FiExternalLink
-                            className={styles.externalLinkIcon}
-                            onClick={() => openWebsiteInNewTab(demo.url)}
-                            title={`Open ${demo.name}`}
-                        />
-                    </div>
-                ))}
+        <Layout>
+            <div className={styles.demos}>
+                <h1>See how the websites we make look and perform.</h1>
+                <div className={styles.demosGrid}>
+                    {demoWebsites.map((demo, index) => (
+                        <div key={index} className={styles.demoCard}>
+                            <h2 className={styles.demoTitle}>{demo.name}</h2>
+                            <iframe
+                                src={demo.url}
+                                title={demo.name}
+                                loading="lazy"
+                                className={styles.demoIframe}
+                                allowFullScreen
+                            ></iframe>
+                            <FiExternalLink
+                                className={styles.externalLinkIcon}
+                                onClick={() => openWebsiteInNewTab(demo.url)}
+                                title={`Open ${demo.name}`}
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
+        </Layout>
     );
 };
 
