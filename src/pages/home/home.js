8e82fb22bd82fb397e81309de9974527e13dfea4
idncod <idncod@protonmail.com>
2024-10-14 14:17:48 +0100

    fix build failing | home.js | it2

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -17,56 +17,102 @@ import ProcessSection from "../../components/processSection/ProcessSection";
 // import Blog from "../blog/Blog";
 import AnimatedChart from "../../components/chart/AnimatedChart";
 import QuoteCalculator from "../../components/Quote/QuoteCalculator";
-import Popup from "../../components/Popup/Popup";
+// import Popup from "../../components/Popup/Popup";
 import ExpoPack from "../../components/ExpoPack/ExpoPack";
 
+
 function Home() {
-	const [showPopup, setShowPopup] = useState(false);
-
-	useEffect(() => {
-		const handleScroll = () => {
-			const middleOfPage = window.innerHeight / 2 + window.scrollY;
-			const pageHeight = document.documentElement.scrollHeight;
-
-			// Trigger when user is around the middle of the page
-			if (middleOfPage >= pageHeight / 2 && !showPopup) {
-				setShowPopup(true);
-			}
-		};
-
-		window.addEventListener('scroll', handleScroll);
-		return () => window.removeEventListener('scroll', handleScroll);
-	}, [showPopup]); // Only reruns if showPopup changes
-
-	const handleClosePopup = () => {
-		setShowPopup(false);
-	};
+	// const [showPopup, setShowPopup] = useState(false);
+	//
+	// const handleScroll = () => {
+	// 	const middleOfPage = window.innerHeight / 2 + window.scrollY;
+	// 	const pageHeight = document.documentElement.scrollHeight;
+	//
+	// 	// Trigger when user is around the middle of the page
+	// 	if (middleOfPage >= pageHeight / 2 && !showPopup) {
+	// 		setShowPopup(true);
+	// 	}
+	// };
+	//
+	// const handleClosePopup = () => {
+	// 	setShowPopup(false);
+	// };
+	//
+	// useEffect(() => {
+	// 	window.addEventListener('scroll', handleScroll);
+	// 	return () => window.removeEventListener('scroll', handleScroll);
+	// }, [showPopup]);
 
 	return (
 		<Layout>
-			<DemoHero />
-			<CarouselStack />
+			<DemoHero/>
+			<CarouselStack/>
 			<div className="home-page-container page-top-space">
-				<ExpoPack />
-				<AnimatedChart />
-				<page-section>
-					<OurServices />
-				</page-section>
-				<QuoteCalculator />
-				<div>
-					<h1>Scroll Down to Trigger Popup</h1>
-					<div style={{ height: '200vh', padding: '20px' }}>
-						<p>Scroll halfway down the page to see the popup.</p>
-					</div>
 
-					{/* Popup with Matrix Animation */}
-					<Popup isOpen={showPopup} onClose={handleClosePopup} />
+				{/*<div className="container relative">*/}
+				{/*	<img src={SpiralImage} className="spiral-image" alt="spiral" />*/}
+
+				{/*	<Typography variant="h1">*/}
+				{/*		Privacy-focused web development optimized for your business.*/}
+				{/*	</Typography>*/}
+
+				{/*	<page-section>*/}
+				{/*		<Typography variant="subtitle1" className="subtitle">*/}
+				{/*			Security is a priority. Get a secure and tested website to prolong the lifespan of your codebase.*/}
+				{/*			With the latest industry best practices and decades of developing experience,*/}
+				{/*			we deliver the results you come for.<br />*/}
+				{/*			Interested? Book a video call now.*/}
+				{/*		</Typography>*/}
+				{/*	</page-section>*/}
+
+				{/*	<page-section className="book-a-call-button-container">*/}
+				{/*		<a href="/schedule-a-call">*/}
+				{/*			<Button className="book-a-call-button">Book a call</Button>*/}
+				{/*		</a>*/}
+				{/*	</page-section>*/}
+
+				{/*<div className="home-page-adv-container">*/}
+				{/*	*/}
+				{/*		<OnePoundAdvert />*/}
+				{/*		<TestCalculator />*/}
+				{/*	*/}
+				{/*</div>*/}
+				<ExpoPack />
+				<AnimatedChart/>
+				<page-section>
+					<OurServices/>
+				</page-section>
+				<QuoteCalculator/>
+				<div>
+				<h1>Scroll Down to Trigger Popup</h1>
+				<div style={{height: '200vh', padding: '20px'}}>
+					<p>Scroll halfway down the page to see the popup.</p>
 				</div>
-				<ProcessSection />
-				<PrimaryForm />
+
+				{/* Popup with Matrix Animation */}
+				<Popup isOpen={showPopup} onClose={handleClosePopup}/>
 			</div>
-		</Layout>
-	);
+			{/*<page-section>*/}
+			{/*	<AboutNuclecode />*/}
+			{/*</page-section>*/}
+
+			{/*<page-section>*/}
+			{/*	<ReadyToStart />*/}
+			{/*</page-section>*/}
+
+			<ProcessSection/>
+
+			<PrimaryForm/>
+
+			{/*<Blog />*/}
+		</div>
+	{/*</div>*/
+	}
+
+</Layout>
+)
+	;
 }
+
 
 export default Home;
